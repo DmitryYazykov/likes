e16a68c10153491806f75072bdcde7252b87f449
Dmitry Yazykov <dmitryyazykov@gmail.com>
2023-03-18 14:44:15 +0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    val likes = readln().toInt()
+    if (likes % 10 == 1 && likes % 100 != 11)
+        println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+}
